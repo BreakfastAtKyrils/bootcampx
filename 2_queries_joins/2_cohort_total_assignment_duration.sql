bfd3fe617c534090dda48769bcafc9f1bb7cabc1
BreakfastAtKyrils <kyrildr@gmail.com>
2022-04-04 19:36:41 +0000

    add TABLES: teachers, assistance_requests

--- 2_queries_joins/2_cohort_total_assignment_duration.sql
+++ 2_queries_joins/2_cohort_total_assignment_duration.sql
@@ -3,4 +3,4 @@ FROM assignment_submissions JOIN students
 ON assignment_submissions.student_id = students.id
 JOIN cohorts
 ON students.cohort_id = cohorts.id
-WHERE FROM cohort.name = 'FEB12';
+
--- 2_queries_joins/2_total_assignment_duration.sql
+++ 2_queries_joins/2_total_assignment_duration.sql
@@ -2,3 +2,6 @@ SELECT SUM(assignment_submissions.duration) as total_duration
 FROM assignment_submissions 
 INNER JOIN students ON assignment_submissions.student_id = students.id
 WHERE students.name = 'Ibrahim Schimmel';
+ON students.cohort_id = cohorts.id
+WHERE FROM cohort.name = 'FEB12';
+
